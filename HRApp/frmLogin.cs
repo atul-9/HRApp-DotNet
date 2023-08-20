0400c9f28bd6ebfd410fff597a8c2f409b189dea
atul-9 <atul09agrawal@gmail.com>
2023-08-20 11:48:57 +0530

    Title Changed

--- HRApp/frmLogin.cs
+++ HRApp/frmLogin.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace HRApp
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
